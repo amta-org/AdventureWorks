bbc6e34086189694f5ba1eaa308235a10e310385
Amanda <52723458+amandahmt@users.noreply.github.com>
2025-09-30 11:21:54 +1000

    Fix duplicate declaration of test_secret
    Removed duplicate declaration of 'test_secret'.

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -3,6 +3,7 @@ using eShop.ServiceDefaults;
 
 var builder = WebApplication.CreateBuilder(args);
 var test_secret = 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;
+var test_secret = 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;
 
 
 
