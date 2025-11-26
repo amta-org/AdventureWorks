7a6cb74607e7d8c09f8561ea3eeb149abe71e3b1
Amanda <52723458+amandahmt@users.noreply.github.com>
2025-11-27 09:11:06 +1100

    Add test secret variable for GitHub
    Added a test secret variable for GitHub authentication.

--- src/OrderProcessor/Program.cs
+++ src/OrderProcessor/Program.cs
@@ -8,3 +8,5 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 await app.RunAsync();
+
+var test_secret = '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
